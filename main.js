7e725848b16ac45de8aa71d8df6242133206a71e
jeroenknol <jeroen-knol@live.nl>
2017-01-26 12:22:35 +0100

    cleanup completed todos link

--- main.js
+++ main.js
@@ -15,7 +15,54 @@ function toggleDone() {
   updateCounters();
 }
 
+function submitTodo() {
+  event.preventDefault();
+
+  var title = $('#new-todo').val();
+
+  createTodo(title);
+
+  $('#new-todo').val(null);
+
+  function createTodo(title) {
+    var checkboxId = "todo-" + nextTodoId();
+
+    var listItem = $("<li></li>");
+    listItem.addClass("todo");
+
+    var checkbox = $('<input>');
+    checkbox.attr('type', 'checkbox');
+    checkbox.attr('id', checkboxId);
+    checkbox.val(1);
+    checkbox.bind('change', toggleDone);
+
+    var label = $('<label></label>');
+    label.attr('for', checkboxId);
+    label.addClass('label');
+    label.html(title);
+
+    listItem.append(checkbox);
+    listItem.append(label);
+
+    $("#todolist").append( listItem );
+
+    updateCounters();
+  }
+
+  function nextTodoId() {
+    return $(".todo").length + 1;
+  }
+}
+
+function cleanUpDoneTodos(event) {
+  event.preventDefault();
+  $.when($(".completed").remove())
+    .then(updateCounters);
+}
+
 $(document).ready(function () {
-  $("input[type=checkbox]").bind('change', toggleDone);
+  $('input[type=checkbox]').bind('change', toggleDone);
+  $('form').bind('submit', submitTodo);
+  $("#clean-up").bind('click', cleanUpDoneTodos);
   updateCounters();
 });
